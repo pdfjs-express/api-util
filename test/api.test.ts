b5f962b3f17c96cbbcc8c5196beb76396362edfa
Logan Bittner <lbittner@pdftron.com>
2020-05-11 08:09:25 -0700

    tests

--- test/api.test.ts
+++ test/api.test.ts
@@ -41,88 +41,99 @@ const XFDF = '<xfdf>';
  */
 describe('API tests', () => {
 
-  it('can call the merge endpoint', async () => {
-    const getParams = mockNextFetch({
-      url: 'https://myfile.com',
-      id: '1234',
-      key: 'aaa||bbb'
+  describe('valid tests', () => {
+    it('can call the merge endpoint', async () => {
+      const getParams = mockNextFetch({
+        url: 'https://myfile.com',
+        id: '1234',
+        key: 'aaa||bbb'
+      });
+  
+      const instance = new APIUtils(KEYS);
+      instance.setFile(BLOB)
+        .setXFDF(XFDF);
+      
+      const resp = await instance.merge();
+      assert.ok(resp);
+      assert.ok(resp?.id);
+      assert.ok(resp?.url);
+      assert.ok(resp?.key);
+      assert.ok(resp?.deleteFile);
+  
+      const params = getParams();
+      assert.equal(params[0], ENDPOINTS.MERGE.url);
     });
-
-    const instance = new APIUtils(KEYS);
-    instance.setFile(BLOB)
-      .setXFDF(XFDF);
-    
-    const resp = await instance.merge();
-    assert.ok(resp);
-    assert.ok(resp?.id);
-    assert.ok(resp?.url);
-    assert.ok(resp?.key);
-    assert.ok(resp?.deleteFile);
-
-    const params = getParams();
-    assert.equal(params[0], ENDPOINTS.MERGE.url);
-  });
-
-  it('can call the set endpoint', async () => {
-    const getParams = mockNextFetch({
-      url: 'https://myfile.com',
-      id: '1234',
-      key: 'aaa||bbb'
+  
+    it('can call the set endpoint', async () => {
+      const getParams = mockNextFetch({
+        url: 'https://myfile.com',
+        id: '1234',
+        key: 'aaa||bbb'
+      });
+  
+      const instance = new APIUtils(KEYS);
+      instance.setFile(BLOB)
+        .setXFDF(XFDF);
+      
+      const resp = await instance.set();
+      assert.ok(resp);
+      assert.ok(resp?.id);
+      assert.ok(resp?.url);
+      assert.ok(resp?.key);
+      assert.ok(resp?.deleteFile);
+  
+      const params = getParams();
+      assert.equal(params[0], ENDPOINTS.SET.url);
     });
-
-    const instance = new APIUtils(KEYS);
-    instance.setFile(BLOB)
-      .setXFDF(XFDF);
-    
-    const resp = await instance.set();
-    assert.ok(resp);
-    assert.ok(resp?.id);
-    assert.ok(resp?.url);
-    assert.ok(resp?.key);
-    assert.ok(resp?.deleteFile);
-
-    const params = getParams();
-    assert.equal(params[0], ENDPOINTS.SET.url);
-  });
-
-  it('can call the extract endpoint', async () => {
-    const getParams = mockNextFetch({
-      xfdf: 'test'
+  
+    it('can call the extract endpoint', async () => {
+      const getParams = mockNextFetch({
+        xfdf: 'test'
+      });
+  
+      const instance = new APIUtils(KEYS);
+      instance.setFile(BLOB);
+      
+      const resp = await instance.extract();
+      assert.ok(resp);
+      assert.ok(resp?.xfdf);
+  
+      const params = getParams();
+      assert.equal(params[0], ENDPOINTS.EXTRACT.url);
     });
-
-    const instance = new APIUtils(KEYS);
-    instance.setFile(BLOB);
-    
-    const resp = await instance.extract();
-    assert.ok(resp);
-    assert.ok(resp?.xfdf);
-
-    const params = getParams();
-    assert.equal(params[0], ENDPOINTS.EXTRACT.url);
-  });
-
-  it('can call the delete endpoint', async () => {
-    mockNextFetch({
-      url: 'https://myfile.com',
-      id: '1234',
-      key: 'aaa||bbb'
-    });
-
-    const instance = new APIUtils(KEYS);
-    instance.setFile(BLOB)
-      .setXFDF(XFDF);
-    
-    const resp = await instance.merge();
-    
-    const getParams = mockNextFetch({});
-
-    await resp.deleteFile();
-
-    const params = getParams();
-    assert.equal(params[0], ENDPOINTS.DELETE.url);
-    assert.ok(params[1].body)
-    const data = params[1].body;
-    assertFormDataContains(data, 'license', '123');
-    assertFormDataContains(data, 'id', '1234');
+  
+    it('can call the delete endpoint', async () => {
+      mockNextFetch({
+        url: 'https://myfile.com',
+        id: '1234',
+        key: 'aaa||bbb'
+      });
+  
+      const instance = new APIUtils(KEYS);
+      instance.setFile(BLOB)
+        .setXFDF(XFDF);
+      
+      const resp = await instance.merge();
+      
+      const getParams = mockNextFetch({});
+  
+      await resp.deleteFile();
+  
+      const params = getParams();
+      assert.equal(params[0], ENDPOINTS.DELETE.url);
+      assert.ok(params[1].body)
+      const data = params[1].body;
+      assertFormDataContains(data, 'license', '123');
+      assertFormDataContains(data, 'id', '1234');
+    })
   })
+
+  describe('error handling', () => {
+
+    it('throws if a large file is added', async () => {
+      
+    })
+
+  })
+
 })
